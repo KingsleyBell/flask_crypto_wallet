48465330d47786c184b4d54009823002e9ade741
Luke Bell <lukekingsleybell@gmail.com>
2019-05-24 11:45:33 +0200

    remove test repo. didnt work.

--- docker-flask-nginx-uwsgi-master/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#start supervisord to bring up webserver and sleep to be sure it's up
-supervisord -n
-
-#Run letsencrypt, if the parameters were given.  Output a log of the letsencrypt attempt
-if [ -n "$1" ]; then
-    python2 /home/flask/conf/setup-https.py $1 > /home/flask/letsencrypt-setup.txt
-fi
