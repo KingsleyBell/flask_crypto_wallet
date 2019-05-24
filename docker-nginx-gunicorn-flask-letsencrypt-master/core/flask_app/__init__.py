c08d8dddfd2934aa05f8c11d4a5fed5aeeb2f908
Luke Bell <lukekingsleybell@gmail.com>
2019-05-24 11:54:35 +0200

    new repo to try.

---
+++ docker-nginx-gunicorn-flask-letsencrypt-master/core/flask_app/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : __init__.py
+# Author            : Matthieu Petiteau <mpetiteau.pro@gmail.com>
+# Date              : Thu  7 Feb 16:23:49 2019
+# Last Modified Date: Thu  7 Feb 16:23:49 2019
+# Last Modified By  : Matthieu Petiteau <mpetiteau.pro@gmail.com>
+
+"""Init app."""
+
+import os
+
+from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+configurations = {
+    'development': 'flask_app.config.DefaultConfig'
+}
+app.config.from_object(configurations[os.getenv('FLASK_ENV')])
+
+import flask_app.serve
